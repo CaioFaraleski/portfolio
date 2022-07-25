141a5a55a353938aadbc5a6a3210ff1ddaf69b0f
Caio Faraleski <caiotadeu_2012@hotmail.com>
2022-07-25 14:14:06 -0300

    add the download img and hover effect at button

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -88,10 +88,11 @@ export function Content() {
                   <h2 className="text-6xl mt-10 pl-1">Front-end Developer</h2>
                   <a
                     href="https://caiofaraleski.github.io/portfolio/src/assets/Caio_Faraleski.pdf"
-                    class="w-fit mt-20 relative border py-[1.4rem] px-[4.2rem] text-3xl text-lightRed tt cursor-pointer before:content-[''] before:absolute before:-z-10 before:top-0 before:left-1/2 before:h-full before:w-0 before:border before:border-x-0 after:content-[''] after:absolute after:-z-10 after:bottom-0 after:left-0 after:h-0 after:w-full after:bg-lightRed hover:text-lightGray hover:delay-500 hover:before:delay-[0s] hover:before:left-0 hover:before:w-full hover:after:bg-lightRed hover:after:delay-[.35s] hover:after:top-0 hover:after:h-full"
+                    class="w-fit flex items-center mt-20 relative border py-[1.4rem] px-[4.2rem] text-3xl text-lightRed tt cursor-pointer before:content-[''] before:absolute before:-z-10 before:top-0 before:left-1/2 before:h-full before:w-0 before:border before:border-x-0 after:content-[''] after:absolute after:-z-10 after:bottom-0 after:left-0 after:h-0 after:w-full after:bg-lightRed hover:text-lightGray hover:delay-500 hover:before:delay-[0s] hover:before:left-0 hover:before:w-full hover:after:bg-lightRed hover:after:delay-[.35s] hover:after:top-0 hover:after:h-full group"
                     download
                   >
                     Resume
+                    <img src="https://caiofaraleski.github.io/portfolio/src/assets/images/download.png" className="downimg filter group-hover:brightness-0 group-hover:invert h-7 ml-5 group-hover:delay-500" />
                   </a>
                 </div>
               ) : null}
