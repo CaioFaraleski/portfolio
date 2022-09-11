e0e229db2b9907a8afa604913c7acc039f035578
Caio Faraleski <caiotadeu_2012@hotmail.com>
2022-09-11 15:39:49 -0300

    add title and description

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,9 @@ export default function Home() {
     <div className='fixed'>
       <Head>
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+        <title>Caio Faraleski</title>
+        <meta name="description" content="Caio Faraleski's portifolio."></meta>
+        <meta name="description" content="A Front End Developer passionate about programming, who will do the best job possible to make your website flawless."></meta>
       </Head>
       <Initial />
     </div>
