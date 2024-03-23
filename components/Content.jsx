4142e44501a688dc797069a3d6047430828a84a1
Caio Faraleski <caiotadeu_2012@hotmail.com>
2024-03-23 12:05:20 -0300

    updating resume

--- components/Content.jsx
+++ components/Content.jsx
@@ -107,7 +107,7 @@ export function Content() {
                   <h1 className="text-lightRed text-[7.5rem] font-semibold mt-4">Caio Faraleski</h1>
                   <h2 className="text-6xl mt-10 pl-1">Front-end Developer</h2>
                   <a
-                    href="/Caio_Faraleski.pdf"
+                    href="/resume.pdf"
                     className="w-fit flex items-center self-center lg:self-start mt-20 relative border py-[1.4rem] px-[4.2rem] text-3xl text-lightRed tt cursor-pointer before:content-[''] before:absolute before:-z-10 before:top-0 before:left-1/2 before:h-full before:w-0 before:border before:border-x-0 after:content-[''] after:absolute after:-z-10 after:bottom-0 after:left-0 after:h-0 after:w-full after:bg-lightRed hover:text-lightGray hover:delay-500 hover:before:delay-[0s] hover:before:left-0 hover:before:w-full hover:after:bg-lightRed hover:after:delay-[.35s] hover:after:top-0 hover:after:h-full group"
                     download
                   >
